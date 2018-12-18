cc6144615fbbb710ee5c9f12feff738910afeb4a
KeyofBlueS <45725677+KeyofBlueS@users.noreply.github.com>
2018-12-18 16:20:08 +0100

    Update Esempio-configurazione-ssh-server.sh

--- Esempio-configurazione-ssh-server.sh
+++ Esempio-configurazione-ssh-server.sh
@@ -22,12 +22,12 @@ export CURRENTIP_FILE=user@server_hostname_current.txt
 # Percorso del file chiave, richiesto per il collegamento tramite key authtentication
 export KEYFILE=~/.ssh/keys/key-ecdsa
 # Porta in ascolto del server ssh (default 22)
-#export SSHPORT=`cat "$CURRENTIP_PATH$CURRENTIP_FILE" | grep "export SSHPORT=" | grep -E -o '([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])'`
+#export SSHPORT="$(cat "$CURRENTIP_PATH$CURRENTIP_FILE" | grep "export SSHPORT=" | grep -Eo '([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])')"
 export SSHPORT=22
 # Porta in cui verrà avviato un Server SOCKS per condividere la connessione del server sul client
 export SOCKSPORT=1080
 # Nome dell'utente presente sul server su cui ci si vuole loggare (non utilizzare "root")
-#export SERVERUSERNAME=`cat "$CURRENTIP_PATH$CURRENTIP_FILE" | grep "export SERVERUSERNAME=" | cut -c23-55`
+#export SERVERUSERNAME="$(cat "$CURRENTIP_PATH$CURRENTIP_FILE" | grep "export SERVERUSERNAME=" | cut -c23-55)"
 export SERVERUSERNAME=server_username
 # $HOSTNAME del server (meramente informativo per una più facile identificazione del server, ma necessario per il montaggio tramite SSHFS)
 export SERVERNAME=server_hostname
@@ -39,22 +39,19 @@ export SERVEMAC="AB:01:CD:23:EF:45 GH:67:IJ:89:KL:10"
 ############################## Impostazioni per il collegamento ssh in locale ##############################
 # Indirizzo preferito (quello più affidabile) per la connessione in locale (SERVERIP_LAN_1) o un IP statico
 # si consiglia comunque di impostare un indirizzo statico sul server
-#export SERVERIP_LAN=`cat $CURRENTIP_PATH$CURRENTIP_FILE | grep SERVERIP_LAN_1 | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'`
+#export SERVERIP_LAN="$(cat $CURRENTIP_PATH$CURRENTIP_FILE | grep SERVERIP_LAN_1 | grep -Eo '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')"
 export SERVERIP_LAN="000.000.000.000"
 # Secondi di attesa prima di provare a ricontattare il server nella rete locale nel caso questo sia irraggiungibile (inserire un valore da 0 a 10)
 export LAN_COUNTDOWN=5
 
 ############################## Impostazioni per il collegamento ssh in remoto ##############################
 # Indirizzo preferito (quello più affidabile) per la connessione in remoto (SERVERIP_INTERNET_1 2 3 o 4) o un IP pubblico statico
-export SERVERIP_INTERNET=`cat "$CURRENTIP_PATH$CURRENTIP_FILE" | grep SERVERIP_INTERNET_2 | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'`
+export SERVERIP_INTERNET="$(cat "$CURRENTIP_PATH$CURRENTIP_FILE" | grep SERVERIP_INTERNET_2 | grep -Eo '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')"
 #export SERVERIP_INTERNET=000.000.000.000
 # Secondi di attesa prima di provare a ricontattare il server remoto nel caso questo sia irraggiungibile (inserire un valore da 0 a 10)
 export INTERNET_COUNTDOWN=10
 
 ############################## Impostazioni per il tipo di collegamento preferito ##############################
-# Tipo di collegamento (meramente informativo, non necessario, inserire qualsiasi valore desiderato) es. LOCALE o REMOTO
-#export TYPE=LOCALE
-export TYPE=REMOTO
 # Indirizzo IP locale o remoto: $SERVERIP_LAN o $SERVERIP_INTERNET
 #export SERVERIP=$SERVERIP_LAN
 export SERVERIP=$SERVERIP_INTERNET
@@ -63,7 +60,7 @@ export SERVERIP=$SERVERIP_INTERNET
 # Tipo di segnale acustico da utilizzare (BEEP, SOX, NULL)
 # BEEP  Imposta il segnale acustico tramite lo speaker interno (richiede beep)
 # SOX   Imposta il segnale acustico tramite la scheda audio (richiede sox)
-# NULL  Disattiva il segnale acustico
+# NULL  Disattiva il segnale acustico (default)
 export AUDIO=BEEP
 # Regola il volume delle segnalazioni acustiche per SOX
 GAIN="-50"
