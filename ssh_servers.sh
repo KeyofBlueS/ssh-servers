b94913c250cfecc8d0110a7ec661a2462f7f9ad0
KeyofBlueS <45725677+KeyofBlueS@users.noreply.github.com>
2019-02-26 06:05:34 +0100

    Update ssh_servers.sh

--- ssh_servers.sh
+++ ssh_servers.sh
@@ -1,9 +1,59 @@
 #!/bin/bash
 
-# Version:    2.5.3
+# Version:    2.5.6
 # Author:     KeyofBlueS
 # Repository: https://github.com/KeyofBlueS/ssh-servers
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
+
+if curl -s github.com > /dev/null; then
+	SCRIPT_LINK="https://raw.githubusercontent.com/KeyofBlueS/ssh-servers/master/ssh_servers.sh"
+	UPSTREAM_VERSION="$(timeout -s SIGTERM 15 curl -L "$SCRIPT_LINK" 2> /dev/null | grep "# Version:" | head -n 1)"
+	LOCAL_VERSION="$(cat "${0}" | grep "# Version:" | head -n 1)"
+	REPOSITORY_LINK="$(cat "${0}" | grep "# Repository:" | head -n 1)"
+	if echo "$LOCAL_VERSION" | grep -q "$UPSTREAM_VERSION"; then
+		echo -n
+	else
+		echo -e "\e[1;33m-----------------------------------------------------------------------------------	
+## ATTENZIONE: questo script non risulta aggiornato alla versione upstream, visita:
+\e[1;32m$REPOSITORY_LINK
+
+\e[1;33m$LOCAL_VERSION (locale)
+\e[1;32m$UPSTREAM_VERSION (upstream)
+\e[1;33m-----------------------------------------------------------------------------------
+
+\e[1;35mPremi invio per aggiornare questo script o attendi 10 secondi per andare avanti normalmente
+\e[1;31m## ATTENZIONE: eventuali modifiche effettuate a questo script verranno perse!!!
+\e[0m
+"
+		if read -t 10 _e; then
+			echo -e "\e[1;34m	Aggiorno questo script...\e[0m"
+			if [[ -L "${0}" ]]; then
+				scriptpath="$(readlink -f "${0}")"
+			else
+				scriptpath="${0}"
+			fi
+			if [ -z "${scriptfolder}" ]; then
+				scriptfolder="${scriptpath}"
+				if ! [[ "${scriptpath}" =~ ^/.*$ ]]; then
+					if ! [[ "${scriptpath}" =~ ^.*/.*$ ]]; then
+					scriptfolder="./"
+					fi
+				fi
+				scriptfolder="${scriptfolder%/*}/"
+				scriptname="${scriptpath##*/}"
+			fi
+			if timeout -s SIGTERM 15 curl -s -o /tmp/"${scriptname}" "$SCRIPT_LINK"; then
+				sudo mv /tmp/flash_update.sh "${scriptfolder}"
+				sudo chown root:root "${scriptfolder}${scriptname}" > /dev/null 2>&1
+				sudo chmod 755 "${scriptfolder}${scriptname}" > /dev/null 2>&1
+				sudo chmod +x "${scriptfolder}${scriptname}" > /dev/null 2>&1
+				echo -e "\e[1;34m	Fatto!
+\e[0m"
+				exec "${scriptfolder}${scriptname}"
+			fi
+		fi
+	fi
+fi
 
 set +a
 #echo -n "Checking dependencies... "
@@ -329,9 +379,9 @@ SERVERIP_START_STEP=serverip_lan
 COUNTDOWN=$LAN_COUNTDOWN
 RESET_COUNTDOWNSTEP=$LANCOUNTSTEP
 if cat "$CURRENTIP" | grep -q "SERVERIP_LAN_"; then
-SERVERIP_STEP=serverip_lan_1
+	SERVERIP_STEP=serverip_lan_1
 else
-SERVERIP_STEP=$LANCOUNTSTEP
+	SERVERIP_STEP=$LANCOUNTSTEP
 fi
 serverip_lan_static
 }
@@ -357,9 +407,9 @@ SERVERIP_START_STEP=serverip_internet
 COUNTDOWN=$INTERNET_COUNTDOWN
 RESET_COUNTDOWNSTEP=$INTERNETCOUNTSTEP
 if cat "$CURRENTIP" | grep -q "SERVERIP_INTERNET_"; then
-SERVERIP_STEP=serverip_internet_1
+	SERVERIP_STEP=serverip_internet_1
 else
-SERVERIP_STEP=$INTERNETCOUNTSTEP
+	SERVERIP_STEP=$INTERNETCOUNTSTEP
 fi
 serverip_internet_static
 }
@@ -619,8 +669,7 @@ echo -e "\e[1;34m
 Provo ad aggiornare gli indirizzi IP...\e[0m"
 rm -f /tmp/$CURRENTIP_FILE.tmp
 wget -q $CURRENTIP_LINK -O /tmp/$CURRENTIP_FILE.tmp
-cat "/tmp/$CURRENTIP_FILE.tmp" | grep -q "SERVER"
-if [ $? = 0 ]; then
+if cat "/tmp/$CURRENTIP_FILE.tmp" | grep -q "SERVER"; then
 	diff -q "$CURRENTIP" "/tmp/$CURRENTIP_FILE.tmp"
 	if [ $? = 0 ]; then
 		echo -e "\e[1;34m ## Era già aggiornato!\e[0m"
@@ -1717,7 +1766,7 @@ givemehelp(){
 echo "
 # ssh-servers
 
-# Version:    2.5.3
+# Version:    2.5.6
 # Author:     KeyofBlueS
 # Repository: https://github.com/KeyofBlueS/ssh-servers
 # License:    GNU General Public License v3.0, https://opensource.org/licenses/GPL-3.0
@@ -1786,30 +1835,22 @@ else
 fi
 }
 
-if [ "$1" = "--default" ]
-then
-   serverip_default
-elif [ "$1" = "--local" ]
-then
-   serverip_lan
-elif [ "$1" = "--remote" ]
-then
-   serverip_internet
-elif [ "$1" = "--manual" ]
-then
-   serverip_manual
-elif [ "$1" = "--ssh" ]
-then
-   manual_ssh
-elif [ "$1" = "--help" ]
-then
-   givemehelp
-elif [ "$1" = "" ]
-then
-   serverip_default
-elif [ "$1" = "--config" ]
-then
-   create_configuration_file
+if [ "$1" = "--default" ]; then
+	serverip_default
+elif [ "$1" = "--local" ]; then
+	serverip_lan
+elif [ "$1" = "--remote" ]; then
+	serverip_internet
+elif [ "$1" = "--manual" ]; then
+	serverip_manual
+elif [ "$1" = "--ssh" ]; then
+	manual_ssh
+elif [ "$1" = "--help" ]; then
+	givemehelp
+elif [ "$1" = "" ]; then
+	serverip_default
+elif [ "$1" = "--config" ]; then
+	create_configuration_file
 else
-   givemehelp
+	givemehelp
 fi
